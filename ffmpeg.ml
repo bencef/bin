ddce682fb6b0cf24499238689b16aae4cb870684
Bence Fabian <bence.fabian@codecool.com>
2021-10-04 11:37:59 +0200

    reformat using ocp-indent

--- ffmpeg.ml
+++ ffmpeg.ml
@@ -33,65 +33,65 @@ module Ffmpeg: sig
   (** [to_string_array builder] returns an array of parameters that can be
       passed to the ffmpeg command. *)
 end =
+struct
+  type t = { inputs: string array list
+           ; pipeline: string array
+           ; outputs: string array }
+
+  type pix_fmt =
+    | Yuv420p
+
+  type color = {r: int; g: int; b: int}
+
+  let string_of_pix_fmt = function
+    | Yuv420p -> "yuv420p"
+
+  let add_source builder params =
+    let handle = builder.inputs |> List.length |> string_of_int in
+    let inputs = params :: builder.inputs in
+    (handle, { builder with inputs })
+
+  module Source =
   struct
-    type t = { inputs: string array list
-             ; pipeline: string array
-             ; outputs: string array }
+    type handle = string
 
-    type pix_fmt =
-      | Yuv420p
+    let file builder path =
+      let params = [| "-i"; path |] in
+      add_source builder params
 
-    type color = {r: int; g: int; b: int}
-
-    let string_of_pix_fmt = function
-      | Yuv420p -> "yuv420p"
-
-    let add_source builder params =
-      let handle = builder.inputs |> List.length |> string_of_int in
-      let inputs = params :: builder.inputs in
-      (handle, { builder with inputs })
-
-    module Source =
-      struct
-        type handle = string
-
-        let file builder path =
-          let params = [| "-i"; path |] in
-          add_source builder params
-
-        let v4l2 ?frame_rate ?pix_fmt builder path =
-          let add_opt value transform template_fun =
-            value
-            |> Option.map transform
-            |> Option.map template_fun
-            |> Option.value ~default:[||] in
-          let params =
-            Array.concat [
-                [| "-f"; "v4l2"|];
-                add_opt frame_rate string_of_int (fun rate -> [| "-framerate"; rate |]);
-                add_opt pix_fmt string_of_pix_fmt (fun pix_fmt ->
-                    [| "-input_format"; "rawvideo"; "-pix_fmt"; pix_fmt |]);
-                [|"-i"; path |]
-              ] in
-          add_source builder params
-      end
-
-    module Filter =
-      struct
-        let color builder color =
-          (* TODO: implement me *)
-          "f1", builder
-      end
-
-    let builder () =
-      let inputs = [] in
-      let pipeline = [||] in
-      let outputs = [||] in
-      { inputs; pipeline; outputs }
-
-    let to_string_array builder =
-      [ builder.inputs |> List.rev |> Array.concat
-      ; builder.pipeline
-      ; builder.outputs ]
-      |> Array.concat
+    let v4l2 ?frame_rate ?pix_fmt builder path =
+      let add_opt value transform template_fun =
+        value
+        |> Option.map transform
+        |> Option.map template_fun
+        |> Option.value ~default:[||] in
+      let params =
+        Array.concat [
+          [| "-f"; "v4l2"|];
+          add_opt frame_rate string_of_int (fun rate -> [| "-framerate"; rate |]);
+          add_opt pix_fmt string_of_pix_fmt (fun pix_fmt ->
+              [| "-input_format"; "rawvideo"; "-pix_fmt"; pix_fmt |]);
+          [|"-i"; path |]
+        ] in
+      add_source builder params
   end
+
+  module Filter =
+  struct
+    let color builder color =
+      (* TODO: implement me *)
+      "f1", builder
+  end
+
+  let builder () =
+    let inputs = [] in
+    let pipeline = [||] in
+    let outputs = [||] in
+    { inputs; pipeline; outputs }
+
+  let to_string_array builder =
+    [ builder.inputs |> List.rev |> Array.concat
+    ; builder.pipeline
+    ; builder.outputs ]
+    |> Array.concat
+end
